e3057c4da2efe575fc4183fde7c78f056199cc91
Choi Jung Hyun <cjhnim@gmail.com>
2022-08-08 17:39:56 +0900

    Password longer than 7 non-alphabetic characters should be weak (refacter)

--- PasswordStrengthChecker/PasswordStrengthChecker/PasswordChecker.cs
+++ PasswordStrengthChecker/PasswordStrengthChecker/PasswordChecker.cs
@@ -8,21 +8,25 @@ namespace PasswordStrengthChecker
     {
         public bool IsAcceptablePassword(string password)
         {
-            if (password.Length > 7)
+            if (password.Length > 7 && IsContainLetter(password))
             {
-                bool isContainLetter = false;
-                foreach(var c in password)
-                {
-                    if (char.IsLetter(c))
-                        isContainLetter = true;
-
-                }
-
-                if(isContainLetter)
-                    return true;
+                return true;
             }
 
             return false;
+        }
+
+        private bool IsContainLetter(string password)
+        {
+            bool isContainLetter = false;
+            foreach (var c in password)
+            {
+                if (char.IsLetter(c))
+                    isContainLetter = true;
+
+            }
+
+            return isContainLetter;
         }
     }
 }
