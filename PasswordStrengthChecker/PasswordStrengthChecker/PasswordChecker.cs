518f24b45304c2e89dc1d02388d63aa7d1576251
Choi Jung Hyun <cjhnim@gmail.com>
2022-08-08 17:20:16 +0900

    initial test projects.

---
+++ PasswordStrengthChecker/PasswordStrengthChecker/PasswordChecker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordStrengthChecker
+{
+    public class PasswordChecker
+    {
+        public bool IsAcceptablePassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PasswordStrengthChecker/PasswordStrengthCheckerTests/PasswordCheckerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordStrengthChecker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordStrengthChecker.Tests
+{
+    [TestClass]
+    public class PasswordCheckerTests
+    {
+        [TestMethod]
+        public void TestNotImplementMethod()
+        {
+            new PasswordChecker().IsAcceptablePassword(null);
+        }
+    }
+}
